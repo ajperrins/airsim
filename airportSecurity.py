73962bd8904b3d03cad0185e6fe68702b4bbec14
Andrew Perrins <aperrins@edmundian.com>
2018-06-28 00:17:38 -0400

    Multi simulations

--- airportSecurity.py
+++ airportSecurity.py
@@ -2,28 +2,31 @@ import simpy
 import random
 
 
-# We vary these two parameters to find suitable values to meet our 15 minute max passenger wait time goal
-NumBoardingPassCheckers = 12
+# We vary these two parameters to find suitable values to meet our 15 minute max
+# passenger wait time goal
+NumBoardingPassCheckers = 8
 NumSecurityScanners = 4
 
 
 Lambda1PassengerArrivalsPerMin = 5  # Problem param
-MuPassengerArrivalRatePerMin = 1/Lambda1PassengerArrivalsPerMin  # Implied problem param
+MuPassengerArrivalRatePerMin = 1 / Lambda1PassengerArrivalsPerMin
 MuBoardingPassServiceTime = 0.75  # Problem param
-SimulationRunTimeInMinutes = 60 * 6
+SimulationRunTimeInMinutes = 100
 
 
 class AirportSecurity(object):
     def __init__(self, env):
         self.env = env
 
-        # There's a single FIFO queue for the next available boarding pass checker, so this
-        # acts as a single SimPy resource with a capacity matching the number of workers
-        self.boardingPassCheckers = simpy.Resource(env, capacity=NumBoardingPassCheckers)
+        # There's a single FIFO queue for the next available boarding pass
+        # checker, so this acts as a single SimPy resource with a capacity matching
+        # the number of workers
+        self.boardingPassCheckers = simpy.Resource(env,
+                                                   capacity=NumBoardingPassCheckers)
 
-        # Security scanners are queued separately (the queueing behavior is one whereby the
-        # shortest queue is used for the next completed boarding-pass traveler). Since the
-        # queues are separate, we set up independent resources
+        # Security scanners are queued separately (the queueing behavior is one
+        # whereby the shortest queue is used for the next completed boarding-pass
+        # traveler). Since the queues are separate, we set up independent resources
         self.securityScanners = []
         i = 0
         while i < NumSecurityScanners:
@@ -37,7 +40,8 @@ class AirportSecurity(object):
         i = 0
         shortest_queue_index = 0
         while i < len(self.securityScanners):
-            if i == 0 or len(self.securityScanners[i].queue) < len(self.securityScanners[shortest_queue_index].queue):
+            if i == 0 or len(self.securityScanners[i].queue) < \
+                    len(self.securityScanners[shortest_queue_index].queue):
                 shortest_queue_index = i
                 person.shortest_queue = i
             i = i + 1
@@ -54,7 +58,7 @@ class AirportSecurity(object):
 
 
 class Passenger(object):
-    def __init__(self, env, num):
+    def __init__(self, env, airport, num):
         self.env = env
         self.arrival_time = 0
         self.started_check_time = 0
@@ -62,54 +66,64 @@ class Passenger(object):
         self.started_security_time = 0
         self.shortest_queue = 0
         self.num = num
+        self.airport = airport
 
     def get_waittime(self):
-        return (self.started_check_time - self.arrival_time) + (self.started_security_time - self.finished_check_time)
+        return (self.started_check_time - self.arrival_time) + \
+               (self.started_security_time - self.finished_check_time)
 
     def go_through_system(self, env):
         global total_passenger_wait_time
 
-        # We track several time-stamps including arrival time to determine duration between events
+        # We track several time-stamps including arrival time to determine
+        # duration between events
         self.arrival_time = self.env.now
-        with airport.boardingPassCheckers.request() as wait_for_checker:
+        with self.airport.boardingPassCheckers.request() as wait_for_checker:
             yield wait_for_checker
 
             self.started_check_time = env.now
 
-            yield env.process(airport.check_passenger())
+            yield env.process(self.airport.check_passenger())
 
             self.finished_check_time = env.now
 
-            shortest_queue_resource = airport.shortest_security_queue(self)
+            shortest_queue_resource = self.airport.shortest_security_queue(self)
             with shortest_queue_resource.request() as wait_for_security:
                 yield wait_for_security
 
-                yield env.process(airport.security_scan())
+                # And now that we've been granted the security resource
+                # ie. gone through the scanner queue, we run the scan
+                # wait simulation which has a uniform distribution wait time
+                yield env.process(self.airport.security_scan())
 
                 self.started_security_time = env.now
-
-                # Passenger.avg_wait_time = (Passenger.avg_wait_time * Passenger.passenger_count) +
                 total_passenger_wait_time = total_passenger_wait_time + self.get_waittime()
-                print("%s wait time %f.... arrived at %f, started check at %f and finished \
-processing at %f, started sec at %f. Shortest queue %i" %
-                  (self,
-                   self.get_waittime(),
-                   self.arrival_time,
-                   self.started_check_time,
-                   self.finished_check_time,
-                   self.started_security_time,
-                   self.shortest_queue))
+
+                # We could track other attributes - like security/check time, total time
+                # and throughput. The following is commented out to prevent excessive verbosity
+                # in the R-markdown output
+
+                #  print("%s wait time %f.... arrived at %f, started check at %f and finished \
+                #   processing at %f, started sec at %f. Shortest queue %i" %
+                #     (self,
+                #      self.get_waittime(),
+                #      self.arrival_time,
+                #      self.started_check_time,
+                #      self.finished_check_time,
+                #      self.started_security_time,
+                #      self.shortest_queue))
 
     def __str__(self):
         return "Person_%d" % self.num
 
-
 def run(env):
     global passenger_count
+
+    airport = AirportSecurity(env)
     # Add passengers into the system on a Poisson distribution with lambda (average interval between
     # arrivals) of `Lambda1PassengerArrivalInterval`
     while True:
-        passenger = Passenger(env, passenger_count)
+        passenger = Passenger(env, airport, passenger_count)
         passenger_count = passenger_count + 1
 
         # The expovariate function produces numbers fitting an exponential distribution where the numbers
@@ -121,8 +135,27 @@ def run(env):
 total_passenger_wait_time = 0
 passenger_count = 0
 
-simpyenv = simpy.Environment()
-airport = AirportSecurity(simpyenv)
-simpyenv.process(run(simpyenv))
-simpyenv.run(until=SimulationRunTimeInMinutes)
-print("\nAVG passenger wait time was %f minutes" % (total_passenger_wait_time/passenger_count))
+
+def run_simulation(sn):
+    global total_passenger_wait_time
+    global passenger_count
+
+    total_passenger_wait_time = 0
+    passenger_count = 0
+
+    simpyenv = simpy.Environment()
+    simpyenv.process(run(simpyenv))
+    simpyenv.run(until=SimulationRunTimeInMinutes)
+    avg_wait_time = total_passenger_wait_time / passenger_count
+    print("Sim %d: avg wait time %f minutes" % (sn, avg_wait_time))
+    return avg_wait_time
+
+
+Simulations = 20
+total = 0
+
+
+for x in range(1, Simulations):
+    total = total + run_simulation(x)
+print("\nAvg simulation wait time %f" % (total / Simulations))
+
